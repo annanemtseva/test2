eb0a181c804c5f5bbd9f7e4df35e5b2ef5ccecba
annanemtseva <nemtseva.anna@gmail.com>
2020-07-10 09:54:00 +0300

    add dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,31 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { JwPaginationComponent } from 'jw-angular-pagination';
 
-import { AppComponent } from "./app.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { UsersListPageComponent } from './users-list-page/users-list-page.component';
+import { UserItemPageComponent } from './user-item-page/user-item-page.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    MainPageComponent,
+    UsersListPageComponent,
+    UserItemPageComponent,
+    JwPaginationComponent
+  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        FormsModule,
+    ],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
